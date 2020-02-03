dcb415772805bdb244444acb13e9a2e69619332f
NakagamiYuta <guguru0014@gmail.com>
2020-02-03 21:24:07 +0900

    [add] 演算子定義に必要な構造体、トレイトを定義(してみた)

--- src/parser/operator.rs
+++ src/parser/operator.rs
@@ -1,3 +1,21 @@
 pub fn test() {
     println!("test called!");
 }
+
+enum OperatorKind {
+    ADD,
+    SUB,
+    MUL,
+    DIV
+}
+
+struct Operator {
+    kind: OperatorKind,
+    val_a: i32,
+    val_b: i32
+}
+
+trait Calc {
+    fn calculate(&self) -> i32;
+}
+
