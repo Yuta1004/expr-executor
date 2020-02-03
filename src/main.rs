8fca74bb750895b6a2cee02dd3f4f16c7f8dbe47
NakagamiYuta <guguru0014@gmail.com>
2020-02-03 20:53:26 +0900

    [add] 引数から対象文字列を受け取る処理を書いた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // 引数
+    let expr: &str;
+    let args = env::args().collect::<Vec<_>>();
+    match args.len() {
+        2 => expr = &args[1],
+        _ => panic!("Error!! Argument size must be 2!")
+    }
+    println!("Received Expr: {}", expr);
 }
