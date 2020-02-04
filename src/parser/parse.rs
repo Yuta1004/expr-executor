13d46d6c04c707b0524c06c152914ef5ada08335
NakagamiYuta <guguru0014@gmail.com>
2020-02-04 20:27:00 +0900

    [add] skip_space実装

---
+++ src/parser/parse.rs
@@ -0,0 +1,18 @@
+fn skip_space(target: &str) -> &str {
+    for (idx, c) in target.chars().enumerate() {
+        match c {
+            ' ' => {},
+            _ => return &target[idx..]
+        }
+    };
+    ""
+}
+
+#[test]
+fn test_skip_space() {
+    assert_eq!(skip_space("test"), "test");
+    assert_eq!(skip_space(" AB C D"), "AB C D");
+    assert_eq!(skip_space("    GitHu b .com"), "GitHu b .com");
+    assert_eq!(skip_space("....... test"), "....... test");
+    assert_eq!(skip_space("  ..//+![2@]0"), "..//+![2@]0");
+}
