9f10b546cbaebbfa2f3ba07994e662bdd505e8e7
NakagamiYuta <guguru0014@gmail.com>
2020-02-04 22:13:51 +0900

    [change] Expressionのval_a, val_bの型をOption<Expr..>からBox<Option<Expr...>>に変更

--- src/parser/expression.rs
+++ src/parser/expression.rs
@@ -9,21 +9,21 @@ pub enum OperatorKind {
     Num
 }
 
-pub struct Expression<'a> {
+pub struct Expression {
     kind: OperatorKind,
-    val_a: Option<&'a Expression<'a>>,
-    val_b: Option<&'a Expression<'a>>,
+    val_a: Box<Option<Expression>>,
+    val_b: Box<Option<Expression>>,
     num: i32
 }
 
-impl<'a> Expression<'a> {
+impl Expression {
 
-    pub fn new(kind: OperatorKind, val_a: &'a Expression<'a>, val_b: &'a Expression<'a>) -> Expression<'a> {
-        Expression{ kind, val_a: Some(val_a), val_b: Some(val_b), num: 0 }
+    pub fn new(kind: OperatorKind, val_a: Expression, val_b: Expression) -> Expression {
+        Expression{ kind, val_a: Box::new(Some(val_a)), val_b: Box::new(Some(val_b)), num: 0 }
     }
 
-    pub fn new_num(val: i32) -> Expression<'a> {
-        Expression{ kind: OperatorKind::Num, val_a: None, val_b: None, num: val }
+    pub fn new_num(val: i32) -> Expression {
+        Expression{ kind: OperatorKind::Num, val_a: Box::new(None), val_b: Box::new(None), num: val }
     }
 
     pub fn calc(&self) -> i32 {
@@ -33,8 +33,14 @@ impl<'a> Expression<'a> {
         }
 
         // Add ~ Div
-        let a = self.val_a.unwrap().calc();
-        let b = self.val_b.unwrap().calc();
+        let a = match &*self.val_a {
+            Some(a) => a.calc(),
+            None => panic!("failed unwrap pval_a")
+        };
+        let b = match &*self.val_b {
+            Some(b) => b.calc(),
+            None => panic!("failed unwrap val_b")
+        };
         match &self.kind {
             OperatorKind::Add => a + b,
             OperatorKind::Sub => a - b,
@@ -54,9 +60,9 @@ fn test_expression() {
     let num_2 = Expression::new_num(2);
     let num_4 = Expression::new_num(4);
     let num_6 = Expression::new_num(6);
-    let expr_add = Expression::new(OperatorKind::Add, &num_1, &num_2);
-    let expr_sub = Expression::new(OperatorKind::Sub, &num_0, &num_4);
-    let expr_mul = Expression::new(OperatorKind::Mul, &expr_add, &expr_sub);
-    let expr_div = Expression::new(OperatorKind::Div, &expr_mul, &num_6);
+    let expr_add = Expression::new(OperatorKind::Add, num_1, num_2);
+    let expr_sub = Expression::new(OperatorKind::Sub, num_0, num_4);
+    let expr_mul = Expression::new(OperatorKind::Mul, expr_add, expr_sub);
+    let expr_div = Expression::new(OperatorKind::Div, expr_mul, num_6);
     assert_eq!(expr_div.calc(), -2);
 }
