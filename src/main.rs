d3e567c42b351018610fc1286154d616c8e0dd15
NakagamiYuta <guguru0014@gmail.com>
2020-02-08 00:22:11 +0900

    [add] read_line関数作成

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use expr_executor::parser::parse::parse;
 
 use std::env;
+use std::io;
 
 fn main() {
     // 引数
@@ -14,4 +15,16 @@ fn main() {
 
     // 計算
     println!("Answer: {}", parse(expr).calc());
+}
+
+/// 標準入力から1行読み込んで返す
+///
+/// # Params
+///
+/// # Return
+/// - String : 入力された文字列
+fn read_line() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).ok();
+    buf.trim().parse().ok().unwrap()
 }
